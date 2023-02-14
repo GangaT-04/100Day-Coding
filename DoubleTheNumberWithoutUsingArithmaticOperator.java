28e00ebf1207b17ce5d0bc4a30fa1be31cbd0b30
GANGA T <99279551+GangaT-04@users.noreply.github.com>
2023-02-14 16:48:44 +0530

    Day30 Program

---
+++ DoubleTheNumberWithoutUsingArithmaticOperator.java
@@ -0,0 +1,8 @@
+package com.talentbattle.java.coding;
+
+public class DoubleTheNumberWithoutUsingArithmaticOperator {
+	public static void main(String[] args) {
+		int a=5;
+		System.out.println("Double the Number is "+(a<<1));
+	}
+}
