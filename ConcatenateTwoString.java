15ea47dcbd5cfa0bb071351999bb83223578b360
GANGA T <99279551+GangaT-04@users.noreply.github.com>
2023-02-14 16:48:16 +0530

    Day29 Program

---
+++ ConcatenateTwoString.java
@@ -0,0 +1,9 @@
+package com.talentbattle.java.coding;
+
+public class ConcatenateTwoString {
+	public static void main(String[] args) {
+		String sen1="Ganga";
+		String sen2=" Thangavelu";
+		System.out.println("Concatenate Two String - "+sen1+sen2);
+	}
+}
